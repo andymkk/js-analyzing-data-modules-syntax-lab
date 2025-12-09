d5453808c92e04341b750a06ae10343dd4235f9e
Andy Mwirigi <akmwirigi23@gmail.com>
2025-12-09 12:45:15 +0300

    info

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
+const Date = require('datejs');
 
-
+function combineUsers(...args) {
+  const combinedObject = {
+    users: []
+  };
+  
+  args.forEach(array => {
+    combinedObject.users = [...combinedObject.users, ...array];
+  });
+  
+  const today = new Date();
+  combinedObject.merge_date = today.toString('M/d/yyyy');
+  
+  return combinedObject;
+}
 
 module.exports = {
   ...(typeof combineUsers !== 'undefined' && { combineUsers })
